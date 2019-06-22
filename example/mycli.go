f7b9e0da5696223c7681ad00f69c8447ba297982
Ben Vezzani <dotvezz@gmail.com>
2019-06-21 20:59:48 -0400

    Basic CLI and shell behavior

---
+++ example/mycli.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"lime/cli"
+)
+
+var commands = []cli.Command{
+	{
+		Keyword: "tell",
+		Commands: []cli.Command{
+			{
+				Keyword: "lie",
+				Func: func(_ []string) error {
+					fmt.Println("The author of this cli likes to eat oranges.")
+					return nil
+				},
+			},
+			{
+				Keyword: "truth",
+				Func: func(_ []string) error {
+					fmt.Println("The author of this cli likes to eat apples.")
+					return nil
+				},
+			},
+		},
+	},
+	{
+		Keyword:     "repeat",
+		Description: "repeats all the words after the command",
+		Examples: []cli.Example{
+			{
+				Input:       "mycli repeat the quick brown fox",
+				Explanation: `outputs "[the quick brown fox]"`,
+			},
+			{
+				Input:       "mycli repeat",
+				Explanation: `returns an error: "there are no words to repeat"`,
+			},
+		},
+		Func: func(args []string) error {
+			if len(args) == 0 {
+				return errors.New("there were no words to repeat")
+			}
+			fmt.Println(args)
+			return nil
+		},
+	},
+}
+
+func main() {
+	mycli := cli.New()
+	_ = mycli.SetCommands(commands...)
+	_ = mycli.Run()
+}
